389adb703c140bec3507822356a8a95ba04655a9
panarin-m04ka <yarniyjack@gmail.com>
2023-11-03 01:54:07 +0300

    finish

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,11 @@
 if [ "$CELERY_FLOWER" = "no" ]
   then
   python manage.py migrate --noinput || exit 1
+  python manage.py migrate --database=logs --noinput || exit 1
   python manage.py makesuperuser || exit 1
 else
   python manage.py migrate --noinput || exit 1
+  python manage.py migrate --database=logs --noinput || exit 1
 fi
 
 exec "$@"
