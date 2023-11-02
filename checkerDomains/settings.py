61088331fcc5e1736461acceea244e594e41f3bd
panarin-m04ka <yarniyjack@gmail.com>
2023-11-03 00:24:30 +0300

    add simple tests

--- checkerDomains/settings.py
+++ checkerDomains/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,10 @@ DATABASES = {
 }
 
 DRF_API_LOGGER_DEFAULT_DATABASE = 'logs'
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES['default']['NAME'] = BASE_DIR / 'db.sqlite.test3'
 
 # For local debug
 # DATABASES = {
