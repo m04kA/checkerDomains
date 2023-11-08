3a5f0e625fe0d8a73c5aadb3170e77e7da78b3df
m04kA <60301384+m04kA@users.noreply.github.com>
2023-11-09 00:26:14 +0300

    change bash script (#12)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,6 @@ if [ "$CELERY_FLOWER" = "no" ]
   python manage.py migrate --database=logs --noinput || exit 1
   python manage.py makesuperuser || exit 1
 else
-  python manage.py migrate --noinput || exit 1
   python manage.py migrate --database=logs --noinput || exit 1
 fi
 
